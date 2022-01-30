da903a6664ad2da88e951e8c58375b813bad7685
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-01-31 02:09:25 +0300

    CategoryAdded

---
+++ Entities/DatabaseEntities/Category.cs
@@ -0,0 +1,15 @@
+﻿using LogBase.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.DatabaseEntities
+{
+    public class Category : AuditableEntity
+    {
+        public string CategoryName { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
