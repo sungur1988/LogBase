7d7dc8fa2b00d9e85d467c768888eebd43eac001
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-01-31 02:09:33 +0300

    ProductAdded

---
+++ Entities/DatabaseEntities/Product.cs
@@ -0,0 +1,17 @@
+﻿using LogBase.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.DatabaseEntities
+{
+    public class Product : AuditableEntity
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
