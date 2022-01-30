4f7a233ade88dd2c2e597a786379ded4ef3c7323
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-01-31 02:09:01 +0300

    BaseEntityAdded

---
+++ Entities/BaseEntities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using LogBase.Core.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.BaseEntities
+{
+    public class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
