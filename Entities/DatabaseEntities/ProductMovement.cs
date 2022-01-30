5dfbf199079aa44381cc1ec345372294a288e895
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-01-31 02:09:55 +0300

    ProductMovementAdded

---
+++ Entities/DatabaseEntities/ProductMovement.cs
@@ -0,0 +1,19 @@
+﻿using LogBase.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.DatabaseEntities
+{
+    public class ProductMovement : AuditableEntity
+    {
+        public int ProductId { get; set; }
+        public string MovementTypeId { get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+        public virtual Product Product { get; set; }
+        public virtual MovementType MovementType { get; set; }
+    }
+}
