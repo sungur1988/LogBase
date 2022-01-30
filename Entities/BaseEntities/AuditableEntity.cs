f2508e884e59d722a4b6c008d3546ee12c044564
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-01-31 02:09:11 +0300

    AuditableEntityAdded

---
+++ Entities/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using LogBase.Core.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity
+    {
+        public int CreatedUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
