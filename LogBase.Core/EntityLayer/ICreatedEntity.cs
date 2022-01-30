4e233ad380834c71975be5468d88858f87106203
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-01-31 02:08:33 +0300

    ICreatedEntityAdded

---
+++ LogBase.Core/EntityLayer/ICreatedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Core.EntityLayer
+{
+    public interface ICreatedEntity
+    {
+        int CreatedUserId { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
