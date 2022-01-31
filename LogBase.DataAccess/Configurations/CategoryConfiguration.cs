41893df54be4392bc0bae00c3968ffa6bf58ecb1
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:08:58 +0300

    CategoryConfigurationAdded

---
+++ LogBase.DataAccess/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using LogBase.Entities.DatabaseEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.CategoryName).IsRequired().HasMaxLength(50);
+        }
+    }
+}
