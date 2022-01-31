1044ed4a1e809a5da66b8b9f6f699ecd20c81e8c
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:08:20 +0300

    ProductMovementRepositoryAdded

---
+++ LogBase.DataAccess/Concrete/ProductMovementRepository.cs
@@ -0,0 +1,18 @@
+﻿using LogBase.DataAccess.Abstract;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess.Concrete
+{
+    public class ProductMovementRepository : BaseRepository<ProductMovement> , IProductMovementRepository
+    {
+        public ProductMovementRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+    }
+}
