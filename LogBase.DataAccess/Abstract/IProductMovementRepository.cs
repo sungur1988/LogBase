e8919c98cf1f2686ae9a88a65f2127a569660dc7
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:04:25 +0300

    IProductMovementRepositoryAdded

---
+++ LogBase.DataAccess/Abstract/IProductMovementRepository.cs
@@ -0,0 +1,14 @@
+﻿using LogBase.Core.DataAccessLayer;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess.Abstract
+{
+    public interface IProductMovementRepository : IRepository<ProductMovement>
+    {
+    }
+}
