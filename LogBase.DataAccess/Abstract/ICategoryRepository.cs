54fce4152be5d36ebda62b1038250666e01d63fb
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:04:03 +0300

    ICategoryRepositoryAdded

---
+++ LogBase.DataAccess/Abstract/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using LogBase.Core.DataAccessLayer;
+using LogBase.Entities.DatabaseEntities;
+
+namespace LogBase.DataAccess.Abstract
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
