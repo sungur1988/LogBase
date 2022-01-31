368aa058fd61c94f829809151e4b06b540771dc3
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:07:49 +0300

    BaseRepositoryAdded

---
+++ LogBase.DataAccess/Concrete/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using LogBase.Core.DataAccessLayer;
+using LogBase.Core.EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess.Concrete
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        protected AppDbContext _appDbContext;
+        private DbSet<TEntity> _dbSet;
+
+
+        public BaseRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = _appDbContext.Set<TEntity>();
+        }
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _appDbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var deletedEntity = _dbSet.Find(id);
+            if (deletedEntity !=null)
+            {
+                _dbSet.Remove(deletedEntity);
+                _appDbContext.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _dbSet.SingleOrDefault(filter);
+        }
+
+        public IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null
+                ? _dbSet.ToList()
+                : _dbSet.Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
