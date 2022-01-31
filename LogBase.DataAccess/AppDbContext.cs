51463564521ee97f2eb744f333fb2808f3b07828
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:09:25 +0300

    AppDbContextAdded

---
+++ LogBase.DataAccess/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using LogBase.DataAccess.Configurations;
+using LogBase.Entities.DatabaseEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category>  Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductMovement> ProductMovements { get; set; }
+        public DbSet<MovementType> MovementTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.Entity<ProductMovement>().Property(x => x.Id).UseIdentityColumn();
+            modelBuilder.Entity<ProductMovement>().HasKey(x => x.Id);
+        }
+    }
+}
