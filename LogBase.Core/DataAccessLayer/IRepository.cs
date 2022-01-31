ccbdf9d8646d49f8a7ab7180d978af4208568570
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:03:46 +0300

    IRepositoryAdded

---
+++ LogBase.Core/DataAccessLayer/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using LogBase.Core.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Core.DataAccessLayer
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+    }
+}
