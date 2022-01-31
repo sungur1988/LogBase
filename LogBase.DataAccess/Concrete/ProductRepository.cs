2c72938ca1c17c4d22adcda185cc7d27a50d30e3
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:08:33 +0300

    ProductRepositoryAdded

---
+++ LogBase.DataAccess/Concrete/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using LogBase.DataAccess.Abstract;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess.Concrete
+{
+    public class ProductRepository : BaseRepository<Product> , IProductRepository
+    {
+        public ProductRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+    }
+}
