de3e40b45d432d7ee1d2d31362bfa24974c574b7
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-01 00:08:01 +0300

    CategoryRepositoryAdded

---
+++ LogBase.DataAccess/Concrete/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using LogBase.DataAccess.Abstract;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.DataAccess.Concrete
+{
+    public class CategoryRepository : BaseRepository<Category> , ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+    }
+}
