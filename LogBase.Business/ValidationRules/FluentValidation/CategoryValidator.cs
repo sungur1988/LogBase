97e518db4d6d6394dc648bbb5465a060dc11d2aa
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 23:03:22 +0300

    CategoryValidatorAdded

---
+++ LogBase.Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.CategoryName).NotNull().MaximumLength(50);
+        }
+    }
+}
