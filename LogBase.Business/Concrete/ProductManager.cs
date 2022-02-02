8f4f6ef1c5742c71e0ac667b071a7c0af84f64e9
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:42:23 +0300

    ProductManagerAdded

---
+++ LogBase.Business/Concrete/ProductManager.cs
@@ -0,0 +1,47 @@
+﻿using LogBase.Business.Abstract;
+using LogBase.Business.Constants;
+using LogBase.Core.Utilities.Results;
+using LogBase.DataAccess.Abstract;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductRepository _productRepository;
+        public ProductManager(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public IResult Add(Product entity)
+        {
+            _productRepository.Add(entity);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IResult Delete(int id)
+        {
+            _productRepository.Delete(id);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        public IDataResult<IEnumerable<Product>> GetList(int? CategoryId)
+        {
+            var data = CategoryId == null
+                ? _productRepository.GetList()
+                : _productRepository.GetList(x => x.CategoryId == CategoryId);
+            return new SuccessDataResult<IEnumerable<Product>>(data, Messages.ProductListed);
+        }
+
+        public IResult Update(Product entity)
+        {
+            _productRepository.Update(entity);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+    }
+}
