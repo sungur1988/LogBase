8d099679be0c390f4c4dafa15b976f0658bef015
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:42:36 +0300

    ProductMovementManagerAdded

---
+++ LogBase.Business/Concrete/ProductMovementManager.cs
@@ -0,0 +1,43 @@
+﻿using LogBase.Business.Abstract;
+using LogBase.Business.Constants;
+using LogBase.Core.Utilities.Results;
+using LogBase.DataAccess.Abstract;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.Concrete
+{
+    public class ProductMovementManager : IProductMovementService
+    {
+        IProductMovementRepository _productMovementRepository;
+        public ProductMovementManager(IProductMovementRepository productMovementRepository)
+        {
+            _productMovementRepository = productMovementRepository;
+        }
+
+        public IResult Add(ProductMovement entity)
+        {
+            _productMovementRepository.Add(entity);
+            return new SuccessResult(Messages.ProductMovementAdded);
+        }
+
+        public IDataResult<IEnumerable<ProductMovement>> GetList(int? productId)
+        {
+            if (productId!=null)
+            {
+                return new SuccessDataResult<IEnumerable<ProductMovement>>(_productMovementRepository.GetList(x => x.ProductId == productId).OrderByDescending(x => x.CreatedDate), Messages.ProductMovementListed);
+            }
+            return new SuccessDataResult<IEnumerable<ProductMovement>>(_productMovementRepository.GetList().OrderByDescending(x => x.CreatedDate));
+        }
+
+        public IDataResult<IEnumerable<ProductMovement>> GetStockTotals()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
