d64f42d1905ece0f68def47d10e7ac166e0d7860
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 23:03:32 +0300

    ProductValidatorAdded

---
+++ LogBase.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.CategoryId).NotNull();
+            RuleFor(p => p.ProductName).NotNull().MaximumLength(100);
+            RuleFor(p => p.Price).GreaterThan(0).NotNull();
+            RuleFor(p => p.CreatedUserId).NotNull();
+            RuleFor(p => p.CreatedDate).NotNull();
+        }
+    }
+}
