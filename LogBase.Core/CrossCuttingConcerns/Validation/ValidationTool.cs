30295976cf6389f20afdda933f7c3b0360cc98eb
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 23:04:32 +0300

    ValidationToolAdded

---
+++ LogBase.Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static  void Validate(IValidator validator,object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var results = validator.Validate(context);
+
+            if (!results.IsValid)
+            {
+                throw new ValidationException(results.Errors);
+            }
+        }
+    }
+}
