e6a4c72cd245f52c2bb238b95aa8a581ac70b539
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:41:51 +0300

    IProductMovementServiceAdded

---
+++ LogBase.Business/Abstract/IProductMovementService.cs
@@ -0,0 +1,18 @@
+﻿using LogBase.Core.Utilities.Results;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.Abstract
+{
+    public interface IProductMovementService
+    {
+        IDataResult<IEnumerable<ProductMovement>> GetList(int? productId);
+        IResult Add(ProductMovement entity);
+        IDataResult<IEnumerable<ProductMovement>> GetStockTotals();
+    }
+}
