11012044d3bbfb3d426040fda5842d08f568a467
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:42:53 +0300

    MessagesAdded

---
+++ LogBase.Business/Constants/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarılı bir şekilde eklendi.";
+        public static string CategoryAdded = "Kategori başarılı bir şekilde eklendi.";
+        public static string CategoryDeleted="Kategori başarılı bir şekilde silindi.";
+        public static string CategoryUpdated = "Kategori başarılı bir şekilde güncellendi.";
+        public static string ProductListed = "Ürünler listelendi.";
+        public static string ProductDeleted = "Ürün başarılı bir şekilde silindi.";
+        public static string ProductUpdated = "Ürün başarılı bir şekilde güncellendi.";
+        public static string CategoryListed = "Kategoriler listelendi.";
+        public static string ProductMovementAdded ="Ürün hareketi başarılı bir şekilde eklendi.";
+        public static string ProductMovementListed = "Ürün hareketleri listelendi.";
+    }
+}
