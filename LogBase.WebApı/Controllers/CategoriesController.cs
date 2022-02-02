70d229dd2471602e2f3e56c660222ec958b16513
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:43:28 +0300

    CatagoriesControllerAdded

---
+++ LogBase.WebApı/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using LogBase.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogBase.WebApı.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public IActionResult GetAll(int? id)
+        {
+            return Ok(_categoryService.GetList());
+        }
+    }
+}
