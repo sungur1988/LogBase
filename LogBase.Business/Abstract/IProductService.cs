0abf08268988a8f1799881b71c63d4e0add6e408
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:42:02 +0300

    IProductServiceAdded

---
+++ LogBase.Business/Abstract/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using LogBase.Core.Utilities.Results;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<IEnumerable<Product>> GetList(int? CategoryId);
+        IResult Add(Product entity);
+        IResult Update(Product entity);
+        IResult Delete(int id);
+    }
+}
