823770b7c3f2008d05c4e6ac133d3ae65caaa8d5
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 16:41:29 +0300

    ICategoryServiceAdded

---
+++ LogBase.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using LogBase.Core.Utilities.Results;
+using LogBase.Entities.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        IDataResult<IEnumerable<Category>> GetList();
+        IResult Add(Category entity);
+        IResult Update(Category entity);
+        IResult Delete(int id);
+    }
+}
