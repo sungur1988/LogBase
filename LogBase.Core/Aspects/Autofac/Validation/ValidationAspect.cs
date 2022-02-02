de7a796eaff494cf04db8f94e53f2ecd18c55354
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-02 23:03:59 +0300

    ValidationAspectAdded

---
+++ LogBase.Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,37 @@
+﻿using Castle.DynamicProxy;
+using FluentValidation;
+using LogBase.Core.CrossCuttingConcerns.Validation;
+using LogBase.Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new Exception("Bu bir doğrulama sınıfı değildir.");
+            }
+            _validatorType = validatorType;
+
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.BaseType.GenericTypeArguments[0];
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+            foreach (var item in entities)
+            {
+                ValidationTool.Validate(validator, item);
+            }
+            
+        }
+    }
+}
