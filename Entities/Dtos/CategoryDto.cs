7817774aabcb677c33d9ba1ee40b30383f4cae28
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-03 20:47:13 +0300

    CategoryDtoAdded

---
+++ Entities/Dtos/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using LogBase.Core.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.Dtos
+{
+    public class CategoryDto : IDto
+    {
+        public string CategoryName { get; set; }
+    }
+}
