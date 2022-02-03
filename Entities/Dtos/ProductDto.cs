26ee4cbcb422cdda15de9aecffc3527959bb7ee2
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-03 20:47:24 +0300

    ProductDtoAdded

---
+++ Entities/Dtos/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using LogBase.Core.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBase.Entities.Dtos
+{
+    public class ProductDto : IDto
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
